8a4070c401a6bd0c4e4c2d0af3c4cbc93c5cf642
Rahul Sharma <rshomerton2001@gmail.com>
2024-05-24 14:22:36 +0530

    Day-3 Assingment React

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Component/Header/Header";
+import Navbar from "./Component/Navbar/Navbar";
+import Hero from "./Component/Hero/Hero";
+import Footer from "./Component/Footer/Footer";
+
+function App() {
+ 
+
+  return (
+    <>
+   <Header/>
+     <Navbar/>
+     <Hero/>
+     <Footer/>
+      
+    </>
+  )
+}
+
+export default App
